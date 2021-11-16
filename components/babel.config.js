51938f2ed2c727ba603f1df860ee739c85523af5
Alex Feinstein <alexander.feinstein@sinch.com>
2021-11-16 10:58:17 +0100

    🛠 build using babel

---
+++ components/babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      { modules: false },
+    ],
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-react',
+      { runtime: 'automatic' },
+    ],
+  ],
+  plugins: [
+    'babel-plugin-html-inline-minifier',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: false,
+      },
+    ],
+  ],
+  generatorOpts: {
+    jsescOption: {
+      quotes: 'single',
+    },
+  },
+  compact: false,
+  comments: false,
+}
---
+++ libs/babel-plugin-html-inline-minifier/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable node/no-sync */
+const cp = require('child_process')
+// const fs = require('fs')
+const p = require('path')
+
+module.exports = function({ types: t }) {
+  return {
+    visitor: {
+      ImportDeclaration: {
+        exit(path, state) {
+          const node = path.node
+
+          if (node.source.value.endsWith('.html')) {
+            const dir = p.dirname(p.resolve(state.file.opts.filename))
+            const absolutePath = p.resolve(dir, node.source.value)
+
+            // const html = fs.readFileSync(absolutePath, 'utf8')
+
+            const minHtml = cp.execSync(`npx html-minifier-terser --collapse-whitespace --keep-closing-slash --minify-css --remove-comments ${absolutePath}`).toString()
+
+            path.replaceWith(t.variableDeclaration('const', [
+              t.variableDeclarator(
+                t.identifier(node.specifiers[0].local.name),
+                t.stringLiteral(minHtml)
+              ),
+            ]))
+          }
+        },
+      },
+    },
+  }
+}
