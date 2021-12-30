740cbaa3704919d6bf843d183ba1f257143633fa
Indravathi Kalepalli <indkal@uk5-pcraclient100.int.clxnetworks.net>
2021-12-30 16:37:25 +0530

    added congrats wizard

--- apps/quickstarts/src/components/PageStepFour.tsx
+++ apps/quickstarts/src/components/PageStepFour.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Congratsbox } from './Congratsbox'
+import { Congratsbox } from './CongratsBox'
 import styles from './Page.module.css'
 //import { usePageControl } from './PageContext'
 import { PageSteps } from './PageSteps'
