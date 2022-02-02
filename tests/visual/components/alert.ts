d028cc7be61b2693bccf478575523dedd2ac79d3
Alex Feinstein <alexander.feinstein@sinch.com>
2022-02-02 11:17:01 +0100

    🛠️ tweak alert visual tests

--- tests/visual/components/alert.ts
+++ tests/visual/components/alert.ts
@@ -1,12 +1,21 @@
 import { test } from '@playwright/test'
 import { makeScreenshotTests } from '../utils'
 
-const shot = makeScreenshotTests(
-  '/alert?width=300&text=Alert%20text',
-  'sinch-alert'
-)
+const longTitle = encodeURIComponent('It has survived not only five centuries, but also the leap into electronic typesetting')
+const longText = encodeURIComponent('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.')
 
-test('type property', shot(async function* ({ $eval }) {
+const withText = makeScreenshotTests('/alert?text=Alert%20text', 'sinch-alert')
+const withTextAndClose = makeScreenshotTests('/alert?text=Alert%20text&dismissable=true', 'sinch-alert')
+const withTextAndButton = makeScreenshotTests('/alert?text=Alert%20text&action=Button', 'sinch-alert')
+const withTextAndButtonAndClose = makeScreenshotTests('/alert?text=Alert%20text&action=Button&dismissable=true', 'sinch-alert')
+const withTextAndButtonAndCloseExpanded = makeScreenshotTests('/alert?width=300&text=Alert%20text&action=Button&dismissable=true', 'sinch-alert')
+const withTextAndButtonAndCloseNarrow = makeScreenshotTests('/alert?width=300&text=Alert%20text%20longer%20title&action=Button&dismissable=true', 'sinch-alert')
+const withTextAndTitle = makeScreenshotTests('/alert?text=Alert%20text&title=Alert%20title', 'sinch-alert')
+const withMultilineTextAndTitleAndButton = makeScreenshotTests(`/alert?width=400&text=${longText}&title=${longTitle}&action=Button&multiline=true`, 'sinch-alert')
+const withMultilineTextAndTitleAndClose = makeScreenshotTests(`/alert?width=400&text=${longText}&title=${longTitle}&dismissable=true&multiline=true`, 'sinch-alert')
+const withMultilineTextAndTitleButtonClose = makeScreenshotTests(`/alert?width=400&text=${longText}&title=${longTitle}&dismissable=true&action=Button&multiline=true`, 'sinch-alert')
+
+test.only('type property', withText(async function* ({ $eval }) {
   await $eval((el) => {
     el.type = 'info'
   })
@@ -28,7 +37,7 @@ test('type property', shot(async function* ({ $eval }) {
   yield { name: 'error' }
 }))
 
-test('type attribute', shot(async function* ({ $eval }) {
+test.only('type attribute', withText(async function* ({ $eval }) {
   await $eval((el) => el.setAttribute('type', 'info'))
   yield { name: 'info' }
   await $eval((el) => el.setAttribute('type', 'success'))
@@ -39,122 +48,68 @@ test('type attribute', shot(async function* ({ $eval }) {
   yield { name: 'error' }
 }))
 
-test('text property', shot(async function* ({ $eval }) {
+test.only('text property', withText(async function* ({ $eval }) {
   await $eval((el) => {
     el.text = 'Updated text'
   })
   yield { name: 'updated' }
 }))
 
-test('text attribute', shot(async function* ({ $eval }) {
+test.only('text attribute', withText(async function* ({ $eval }) {
   await $eval((el) => {
     el.setAttribute('text', 'Updated text')
   })
   yield { name: 'updated' }
 }))
 
-test('dismissable attribute', shot(async function* ({ $eval }) {
-  await $eval((el) => el.setAttribute('dismissable', ''))
+test.only('text close', withTextAndClose(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('text button', withTextAndButton(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('text button close', withTextAndButtonAndClose(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('text button close expanded', withTextAndButtonAndCloseExpanded(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('text button close narrow', withTextAndButtonAndCloseNarrow(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('multiline title text button', withMultilineTextAndTitleAndButton(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('multiline title text close', withMultilineTextAndTitleAndClose(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('multiline title text button close', withMultilineTextAndTitleButtonClose(async function* () {
+  yield { name: 'shot' }
+}))
+
+test.only('multiline property', withTextAndTitle(async function* ({ $eval }) {
+  await $eval((el) => {
+    el.multiline = true
+  })
   yield { name: 'enabled' }
 
-  await $eval((el) => el.removeAttribute('dismissable'))
+  await $eval((el) => {
+    el.multiline = false
+  })
   yield { name: 'disabled' }
 }))
 
-test('dismissable property', shot(async function* ({ $eval }) {
-  await $eval((el) => {
-    el.dismissable = true
-  })
+test.only('multiline attribute', withTextAndTitle(async function* ({ $eval }) {
+  await $eval((el) => el.setAttribute('multiline', ''))
   yield { name: 'enabled' }
 
-  await $eval((el) => {
-    el.dismissable = false
-  })
+  await $eval((el) => el.removeAttribute('multiline'))
   yield { name: 'disabled' }
-}))
-
-test('multiline property', shot(async function* ({ $eval }) {
-  await $eval((el) => {
-    el.title = ''
-    el.actionText = 'Action'
-    el.multiline = false
-  })
-  yield { name: 'action-no-multiline' }
-
-  await $eval((el) => {
-    el.title = 'Alert Title'
-    el.actionText = ''
-    el.multiline = false
-  })
-  yield { name: 'title-no-multiline' }
-
-  await $eval((el) => {
-    el.title = 'Alert Title'
-    el.actionText = 'Action'
-    el.multiline = true
-  })
-  yield { name: 'title-action-multiline' }
-
-  await $eval((el) => {
-    el.title = ''
-    el.actionText = 'Action'
-    el.multiline = true
-  })
-  yield { name: 'action-multiline' }
-  await $eval((el) => {
-    el.title = 'Alert Title'
-    el.actionText = ''
-    el.multiline = true
-  })
-  yield { name: 'title-multiline' }
-  await $eval((el) => {
-    el.title = 'Alert Title'
-    el.actionText = 'Action'
-    el.dismissable = true
-    el.multiline = true
-  })
-  yield { name: 'title-action-dismissable-multiline' }
-}))
-
-test('multiline attribute', shot(async function* ({ $eval }) {
-  await $eval((el) => {
-    el.setAttribute('title', '')
-    el.setAttribute('actiontext', 'Action')
-  })
-  yield { name: 'action-no-multiline' }
-
-  await $eval((el) => {
-    el.setAttribute('actiontext', '')
-    el.setAttribute('title', 'Alert Title')
-  })
-  yield { name: 'title-no-multiline' }
-
-  await $eval((el) => {
-    el.setAttribute('multiline', '')
-    el.setAttribute('title', 'Alert Title')
-    el.setAttribute('actiontext', 'Action')
-  })
-  yield { name: 'title-action-multiline' }
-
-  await $eval((el) => {
-    el.removeAttribute('title')
-    el.setAttribute('actiontext', 'Action')
-    el.setAttribute('multiline', '')
-  })
-  yield { name: 'action-multiline' }
-
-  await $eval((el) => {
-    el.removeAttribute('actiontext')
-    el.setAttribute('title', 'Alert Title')
-    el.setAttribute('multiline', '')
-  })
-  yield { name: 'title-multiline' }
-
-  await $eval((el) => {
-    el.setAttribute('title', 'Alert Title')
-    el.setAttribute('actiontext', 'Action')
-    el.setAttribute('dismissable', '')
-    el.setAttribute('multiline', '')
-  })
-  yield { name: 'title-action-dismissable-multiline' }
 }))
