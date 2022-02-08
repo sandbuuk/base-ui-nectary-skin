1600155c41a0edd2b51d88915c65ea8e4994768b
Alex Feinstein <alexander.feinstein@sinch.com>
2022-02-08 15:28:05 +0100

    🛠️ remove React handler call utility

--- components/utils.ts
+++ components/utils.ts
@@ -1,17 +1,5 @@
 export type TEventHandler = (arg?: any) => void
 
-export const getEventHandler = ($element: Element, handlerName: string): TEventHandler | null => {
-  // https://github.com/facebook/react/issues/7901
-  for (const key in $element) {
-    if (key.startsWith('__reactProps$')) {
-      // @ts-ignore
-      return $element[key][handlerName]
-    }
-  }
-
-  return null
-}
-
 export const defineCustomElement = (name: string, constructor: CustomElementConstructor): void => {
   if (customElements.get(name) == null) {
     customElements.define(name, constructor)
