de1318ff087111c43222abb03bd35f94d23223a6
Alex Feinstein <alexander.feinstein@sinch.com>
2022-05-31 14:40:27 +0200

    ➕ add `sinch-avatar-status` and `sinch-avatar-badge`

---
+++ components/avatar-badge/index.ts
@@ -0,0 +1,72 @@
+import '../icon/cancel'
+import {
+  defineCustomElement,
+  getAttribute,
+  setClass,
+  updateAttribute,
+} from '../utils'
+import templateHTML from './template.html'
+import type { TSinchElementReact } from '../types'
+
+const template = document.createElement('template')
+
+template.innerHTML = templateHTML
+
+defineCustomElement('sinch-avatar-badge', class extends HTMLElement {
+  #$text: HTMLSpanElement
+
+  constructor() {
+    super()
+
+    const shadowRoot = this.attachShadow({
+      mode: 'closed',
+    })
+
+    shadowRoot.appendChild(template.content.cloneNode(true))
+
+    this.#$text = shadowRoot.querySelector('#text')!
+  }
+
+  get text() {
+    return getAttribute(this, 'text', '')
+  }
+
+  set text(value: string) {
+    updateAttribute(this, 'text', value)
+  }
+
+  static get observedAttributes() {
+    return ['text']
+  }
+
+  attributeChangedCallback(name: string, _: string | null, newVal: string | null) {
+    switch (name) {
+      case 'text': {
+        this.#$text.textContent = newVal
+        setClass(this.#$text, 'long', newVal !== null && newVal.length > 1)
+
+        break
+      }
+    }
+  }
+})
+
+export type TSinchAvatarBadgeElement = HTMLElement & {
+  text: string,
+}
+
+export type TSinchAvatarBadgeReact = TSinchElementReact<TSinchAvatarBadgeElement> & {
+  text: string,
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'sinch-avatar-badge': TSinchAvatarBadgeReact,
+    }
+  }
+
+  interface HTMLElementTagNameMap {
+    'sinch-avatar-badge': TSinchAvatarBadgeElement,
+  }
+}
--- tests/apps/react/src/components/Avatar.tsx
+++ tests/apps/react/src/components/Avatar.tsx
@@ -9,6 +9,8 @@ export const Avatar: FC<TAvatar> = ({ search }) => {
   const src = search.get('src') ?? undefined
   const background: any = search.get('bg') ?? undefined
   const size: any = search.get('size') ?? undefined
+  const badgeText = search.get('badge')
+  const statusColor: any = search.get('status')
 
   return (
     <sinch-avatar
@@ -16,6 +18,9 @@ export const Avatar: FC<TAvatar> = ({ search }) => {
       alt={alt}
       size={size}
       background={background}
-    />
+    >
+      {badgeText !== null && <sinch-avatar-badge slot="badge" text={badgeText}/>}
+      {statusColor !== null && <sinch-avatar-status slot="status" color={statusColor}/>}
+    </sinch-avatar>
   )
 }
