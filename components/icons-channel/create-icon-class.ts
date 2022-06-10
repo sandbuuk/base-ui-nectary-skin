21fa754cd2a7ffecd6352d2baef0426a294ff904
Alex Feinstein <alexander.feinstein@sinch.com>
2022-06-10 15:52:42 +0200

    ➕ add channel icons

---
+++ components/icons-channel/create-icon-class.ts
@@ -0,0 +1,59 @@
+import { getIntegerAttribute, updateAttribute, updateIntegerAttribute } from '../utils'
+import iconStylesHtml from './icon-styles.html'
+
+const DEFAULT_SIZE = 48
+const MIN_SIZE = 4
+const MAX_SIZE = 256
+
+export const createIconClass = (templateHTML: string) => {
+  const template = document.createElement('template')
+
+  template.innerHTML = iconStylesHtml + templateHTML
+
+  return class extends HTMLElement {
+    $svg: SVGElement
+
+    constructor() {
+      super()
+
+      const shadowRoot = this.attachShadow({
+        mode: 'closed',
+      })
+
+      shadowRoot.appendChild(template.content.cloneNode(true))
+
+      this.$svg = shadowRoot.querySelector('svg')!
+    }
+
+    static get observedAttributes() {
+      return ['size']
+    }
+
+    set size(value: number) {
+      // Validation is handled in attributeChangeCallback
+      updateAttribute(this, 'size', value)
+    }
+
+    get size() {
+      return getIntegerAttribute(this, 'size', DEFAULT_SIZE)
+    }
+
+    connectedCallback() {
+      updateAttribute(this.$svg, 'preserveAspectRatio', 'xMinYMin meet')
+
+      if (!this.hasAttribute('size')) {
+        updateAttribute(this, 'size', DEFAULT_SIZE)
+      }
+    }
+
+    attributeChangedCallback(name: string, _: string | null, newVal: string | null) {
+      switch (name) {
+        case 'size': {
+          updateIntegerAttribute(this.$svg, 'height', newVal, { min: MIN_SIZE, max: MAX_SIZE })
+
+          break
+        }
+      }
+    }
+  }
+}
---
+++ components/icons-channel/facebook-messenger/index.ts
@@ -0,0 +1,18 @@
+import { defineCustomElement } from '../../utils'
+import { createIconClass } from '../create-icon-class'
+import templateHTML from './template.html'
+import type { TSinchIconChannelElement, TSinchIconChannelReact } from '../types'
+
+defineCustomElement('sinch-icon-channel-facebook-messenger', createIconClass(templateHTML))
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'sinch-icon-channel-facebook-messenger': TSinchIconChannelReact,
+    }
+  }
+
+  interface HTMLElementTagNameMap {
+    'sinch-icon-channel-facebook-messenger': TSinchIconChannelElement,
+  }
+}
