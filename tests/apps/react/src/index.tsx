b84f3064ccba5c596ec65e548c0a370547311fa2
Alex Feinstein <alexander.feinstein@sinch.com>
2022-06-14 17:52:09 +0200

    🛠️ use ShadowRoot in React tests

--- tests/apps/react/src/index.tsx
+++ tests/apps/react/src/index.tsx
@@ -1,8 +1,31 @@
+import '@webcomponents/scoped-custom-element-registry'
+import { getNectaryCustomRegistry } from '@sinch-engage/nectary/utils'
 import { render } from 'react-dom'
 import { App } from './App'
 import 'axe-core'
 
+const createShadowRoot = (element: HTMLElement, registry: CustomElementRegistry) => {
+  const shadowRoot = element.attachShadow({
+    mode: 'open',
+    customElements: registry,
+  })
+
+  const appElement = shadowRoot.appendChild(document.createElement('div'))
+
+  Object.defineProperty(appElement, 'ownerDocument', { value: shadowRoot })
+  Object.defineProperty(shadowRoot, 'createTextNode', {
+    value: document.createTextNode.bind(shadowRoot.ownerDocument),
+  })
+  Object.defineProperty(shadowRoot, 'createElementNS', {
+    value: document.createElementNS.bind(shadowRoot.ownerDocument),
+  })
+
+  return appElement
+}
+
+const appElement = createShadowRoot(document.getElementById('app')!, getNectaryCustomRegistry())
+
 render(
   <App/>,
-  document.getElementById('app')
+  appElement
 )
