fd1cc91448baaeec497fb5727f3e9440b53d6180
Alex Feinstein <alexander.feinstein@sinch.com>
2022-06-16 17:03:55 +0200

    🛠️ tweak test retries

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -80,8 +80,8 @@ const config: PlaywrightTestConfig = {
     threshold: 60000,
   },
   fullyParallel: true,
-  workers: process.env.CI != null ? 1 : 0,
-  // retries: 1,
+  workers: process.env.CI != null ? 1 : 3,
+  retries: 1,
   maxFailures: process.env.CI != null ? 1 : 0,
   expect: {
     toMatchSnapshot: {
