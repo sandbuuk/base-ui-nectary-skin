b40e18b7bab2dbdbca981ca0d4ee2b0a76427b58
Kir Belevich <kir.belevich@sinch.com>
2022-07-07 15:49:19 +0200

    🛠 website: tweak MDX components

--- website/src/components/mdx-components.tsx
+++ website/src/components/mdx-components.tsx
@@ -1,19 +1,47 @@
 import '@sinch-engage/nectary/title'
+import '@sinch-engage/nectary/text'
+import '@sinch-engage/nectary/link'
+import { Children } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import type { MDXComponents } from 'mdx/types'
 
 export const mdxComponents: MDXComponents = {
   h1: ({ children }) => (
-    <sinch-title type="xl" aria-level="1" text={children as string}/>
+    <sinch-title type="xl" level="1" text={children as string}/>
   ),
   h2: ({ children }) => (
-    <sinch-title type="l" aria-level="2" text={children as string}/>
+    <sinch-title type="l" level="2" text={children as string}/>
   ),
   h3: ({ children }) => (
-    <sinch-title type="m" aria-level="3" text={children as string}/>
+    <sinch-title type="m" level="3" text={children as string}/>
   ),
-  code: ({ children, className }) => {
+  h4: ({ children }) => (
+    <sinch-title type="m" level="4" text={children as string}/>
+  ),
+  h5: ({ children }) => (
+    <sinch-title type="m" level="5" text={children as string}/>
+  ),
+  h6: ({ children }) => (
+    <sinch-title type="m" level="6" text={children as string}/>
+  ),
+  p: ({ children }) => (
+    <p>
+      {Children.map(children, (child) => (
+        typeof child === 'string'
+          ? <sinch-text inline type="m" text={child}/>
+          : child
+      ))}
+    </p>
+  ),
+  a: ({ href = '#', children }) => {
+    const isExternal = href.startsWith('http')
+
+    return (
+      <sinch-link href={href} text={children as string} external={isExternal}/>
+    )
+  },
+  code: ({ className, children }) => {
     if (className == null) {
       return (
         <code>{children}</code>
