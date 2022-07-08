b2fd5b92ae69f5a129a3a9a92d13619edd05d6e9
Kir Belevich <kir.belevich@sinch.com>
2022-07-08 15:07:51 +0200

    🛠 website: add anchors to titles

--- website/src/components/mdx-components.tsx
+++ website/src/components/mdx-components.tsx
@@ -4,27 +4,74 @@ import '@sinch-engage/nectary/link'
 import { Children } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
+// @ts-expect-error
+import { convert as slugify } from 'url-slug'
 import type { MDXComponents } from 'mdx/types'
 
 export const mdxComponents: MDXComponents = {
-  h1: ({ children }) => (
-    <sinch-title type="xl" level="1" text={children as string}/>
-  ),
-  h2: ({ children }) => (
-    <sinch-title type="l" level="2" text={children as string}/>
-  ),
-  h3: ({ children }) => (
-    <sinch-title type="m" level="3" text={children as string}/>
-  ),
-  h4: ({ children }) => (
-    <sinch-title type="m" level="4" text={children as string}/>
-  ),
-  h5: ({ children }) => (
-    <sinch-title type="m" level="5" text={children as string}/>
-  ),
-  h6: ({ children }) => (
-    <sinch-title type="m" level="6" text={children as string}/>
-  ),
+  h1: ({ children }) => {
+    const text = children as string
+    const id = slugify(text)
+
+    return (
+      <div className="title" id={id}>
+        <a className="title-anchor" href={`#${id}`}>#</a>
+        <sinch-title type="xl" level="1" text={text}/>
+      </div>
+    )
+  },
+  h2: ({ children }) => {
+    const text = children as string
+    const id = slugify(text)
+
+    return (
+      <div className="title" id={id}>
+        <a className="title-anchor" href={`#${id}`}>#</a>
+        <sinch-title type="l" level="2" text={text}/>
+      </div>
+    )
+  },
+  h3: ({ children }) => {
+    const text = children as string
+    const id = slugify(text)
+
+    return (
+      <div className="title" id={id}>
+        <a className="title-anchor" href={`#${id}`}>#</a>
+        <sinch-title type="m" level="3" text={text}/>
+      </div>
+    )
+  },
+  h4: ({ children }) => {
+    const text = children as string
+    const id = slugify(text)
+
+    return (
+      <div className="title" id={id}>
+        <a className="title-anchor" href={`#${id}`}>#</a>
+        <sinch-title type="s" level="4" text={text}/>
+      </div>
+    )
+  },
+  h5: ({ children }) => {
+    const text = children as string
+    const id = slugify(text)
+
+    return (
+      <div className="title" id={id}>
+        <a className="title-anchor" href={`#${id}`}>#</a>
+        <sinch-title type="xs" level="5" text={text}/>
+      </div>
+    )
+  },
+  h6: ({ children }) => {
+    const text = children as string
+    const id = slugify(text)
+
+    return (
+      <sinch-title id={id} type="xs" level="6" text={text}/>
+    )
+  },
   p: ({ children }) => (
     <sinch-text type="m">
       {Children.map(children, (child) => (
