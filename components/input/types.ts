8a3d7f25740b7f75d255d9b4e04e84f32f7e4735
Kir Belevich <kir.belevich@sinch.com>
2022-07-13 17:00:42 +0200

    🛠 refactor `sinch-input` types

--- components/input/types.ts
+++ components/input/types.ts
@@ -1,13 +1,16 @@
 import type { TSinchElementReact } from '../types'
-import type { inputTypes } from './utils'
 import type { SyntheticEvent } from 'react'
 
-export type TTextInputType = typeof inputTypes[number]
+export type TSinchInputType = 'text' | 'password'
 
 export type TSinchInputElement = HTMLElement & {
-  type: TTextInputType,
+  /** Text field type, similar to a native HTML input types */
+  type: TSinchInputType,
+  /** Value */
   value: string,
+  /** Label */
   label: string,
+  /** Text that appears in the text field when it has no value set */
   placeholder: string | null,
   optionalText: string | null,
   invalidText: string | null,
@@ -19,7 +22,7 @@ export type TSinchInputElement = HTMLElement & {
   focus(): void,
   blur(): void,
   addEventListener(type: 'change', listener: (e: CustomEvent<string>) => void): void,
-  setAttribute(name: 'type', value: TTextInputType): void,
+  setAttribute(name: 'type', value: TSinchInputType): void,
   setAttribute(name: 'value', value: string): void,
   setAttribute(name: 'label', value: string): void,
   setAttribute(name: 'placeholder', value: string): void,
@@ -30,14 +33,24 @@ export type TSinchInputElement = HTMLElement & {
 }
 
 export type TSinchInputReact = TSinchElementReact<TSinchInputElement> & {
-  type?: TTextInputType,
+  /** Controlled value, doesn't change on its own and requres an onChange-value state loop */
   value: string,
+  /** Label that shows in UI */
   label: string,
-  placeholder?: string,
-  optionalText?: string,
-  invalidText?: string,
-  additionalText?: string,
-  disabled?: boolean,
+  /** Label that is used for a11y – might be different from `label` */
   'aria-label': string,
+  /** Text field type, `text` by default */
+  type?: TSinchInputType,
+  /** Text that appears in the text field when it has no value set */
+  placeholder?: string,
+  /** Optional text */
+  optionalText?: string,
+  /** Additional text */
+  additionalText?: string,
+  /** Invalid text */
+  invalidText?: string,
+  /** Disabled */
+  disabled?: boolean,
+  /** Change value handler */
   onChange: (e: SyntheticEvent<TSinchInputElement, CustomEvent<string>>) => void,
 }
