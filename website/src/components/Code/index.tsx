f0e3590a8e6690f6c76130d5a4d557b4fc15e09f
Kir Belevich <kir.belevich@sinch.com>
2022-07-26 11:08:06 +0200

    🛠 website: tweak syntax hightlighter font family

--- website/src/components/Code/index.tsx
+++ website/src/components/Code/index.tsx
@@ -1,7 +1,19 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import './styles.css'
-import type { FC } from 'react'
+import type { CSSProperties, FC } from 'react'
+
+const style: Record<string, CSSProperties> = {
+  ...dracula,
+  'code[class*="language-"]': {
+    ...dracula['"code[class*="language-"]"'],
+    fontFamily: 'monospace',
+  },
+  'pre[class*="language-"]': {
+    ...dracula['pre[class*="language-"]'],
+    fontFamily: 'monospace',
+  },
+}
 
 export type TCode = {
   src: string,
@@ -11,7 +23,7 @@ export type TCode = {
 export const Code: FC<TCode> = ({ src, title }) => (
   <details className="example-code">
     <summary>{title}</summary>
-    <SyntaxHighlighter language="tsx" style={dracula}>
+    <SyntaxHighlighter language="tsx" style={style}>
       {src}
     </SyntaxHighlighter>
   </details>
