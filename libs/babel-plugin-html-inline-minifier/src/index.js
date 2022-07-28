50629d2339f33cf10b83646900140e15ca65749e
Alex Feinstein <alexander.feinstein@sinch.com>
2022-07-28 17:09:02 +0200

    🛠️ fix components build

--- libs/babel-plugin-html-inline-minifier/src/index.js
+++ libs/babel-plugin-html-inline-minifier/src/index.js
@@ -3,7 +3,7 @@ const cp = require('child_process')
 // const fs = require('fs')
 const p = require('path')
 
-const execPath = p.resolve(require.resolve('html-minifier-terser'), '../../node_modules/.bin/html-minifier-terser')
+const execPath = p.resolve(require.resolve('html-minifier-terser'), '../../cli.js')
 
 module.exports = function({ types: t }) {
   return {
@@ -15,7 +15,7 @@ module.exports = function({ types: t }) {
           if (node.source.value.endsWith('.html')) {
             const dir = p.dirname(p.resolve(state.file.opts.filename))
             const absolutePath = p.resolve(dir, node.source.value)
-            const minHtml = cp.execSync(`${execPath} --collapse-whitespace --keep-closing-slash --minify-css --remove-comments ${absolutePath}`).toString()
+            const minHtml = cp.execSync(`node ${execPath} --collapse-whitespace --keep-closing-slash --minify-css --remove-comments ${absolutePath}`).toString()
 
             path.replaceWith(t.variableDeclaration('const', [
               t.variableDeclarator(
