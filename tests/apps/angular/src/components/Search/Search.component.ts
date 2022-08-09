0bee51622e32cdbb435b444030c020fadb79e3f6
Alex Feinstein <alexander.feinstein@sinch.com>
2022-08-09 13:55:58 +0200

    🛠️ add search example

--- tests/apps/angular/src/components/Search/Search.component.ts
+++ tests/apps/angular/src/components/Search/Search.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core'
-import '@sinch-engage/nectary/search'
-import '@sinch-engage/nectary/search-option'
+import '@sinch-engage/nectary/input'
+import '@sinch-engage/nectary/help-tooltip'
+import '@sinch-engage/nectary/action-menu'
+import '@sinch-engage/nectary/action-menu-option'
+import '@sinch-engage/nectary/icon-button'
+import '@sinch-engage/nectary/icons/search'
+import '@sinch-engage/nectary/icons/close'
 
 @Component({
   selector: 'search-component',
@@ -9,27 +14,44 @@ import '@sinch-engage/nectary/search-option'
 })
 
 export class SearchComponent {
+  isOpen = false
   value: string
-  label: string | null
-  placeholder: string | null
-  isControlled: boolean
+  labelText: string | null
+  optionalText: string | null
+  additionalText: string | null
+  invalidText: string | null
+  placeholderText: string | null
+  tooltipText: string | null
+  isDisabled: boolean
   maxVisibleItems: number | null
+  options = [
+    'Option 1 value long long long',
+    'Option 2',
+    'Option 3',
+    'Option 4',
+  ]
 
   constructor() {
     const url = new URL(location.href)
     this.value = url.searchParams.get('value') ?? ''
-    this.label = url.searchParams.get('label')
-    this.placeholder = url.searchParams.get('placeholder')
-    this.isControlled = url.searchParams.get('uncontrolled') === null
+    this.labelText = url.searchParams.get('label')
+    this.optionalText = url.searchParams.get('optional')
+    this.additionalText = url.searchParams.get('additional')
+    this.invalidText = url.searchParams.get('invalid')
+    this.placeholderText = url.searchParams.get('placeholder')
+    this.tooltipText = url.searchParams.get('tooltip')
+    this.isDisabled = url.searchParams.get('disabled') != null
 
     const numVisibleValue = url.searchParams.get('maxvisibleitems')
     this.maxVisibleItems = numVisibleValue !== null ? parseInt(numVisibleValue) : null
   }
-
   onChange(e: Event) {
-    if (this.isControlled) {
-      this.value = (e as CustomEvent).detail
+    this.value = (e as CustomEvent).detail
+
+    if (this.value.length >= 5) {
+      this.isOpen = true
     }
+
     window.dispatchEvent(new CustomEvent('sinch-search-change', {detail: (e as CustomEvent).detail}))
   }
   onFocus() {
@@ -37,5 +59,19 @@ export class SearchComponent {
   }
   onBlur() {
     window.dispatchEvent(new CustomEvent('sinch-search-blur'))
+    this.isOpen = false
+  }
+  onClear() {
+    this.value = ''
+    this.isOpen = false
+  }
+  onClose() {
+    this.isOpen = false
+  }
+  onOptionClick(text: string) {
+    window.dispatchEvent(new CustomEvent('sinch-search-change', { detail: text }))
+
+    this.value = text
+    this.isOpen = false
   }
 }
