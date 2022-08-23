2dc9a23012fd6d01694b9aa291b825d2fe5fb4ee
Kir Belevich <kir.belevich@sinch.com>
2022-08-23 11:39:39 +0200

    📝 add docs for HorizontalStepper

--- components/horizontal-stepper-item/types.ts
+++ components/horizontal-stepper-item/types.ts
@@ -3,16 +3,25 @@ import type { TSinchElementReact } from '../types'
 export type TSinchHorizontalStepperStatusType = 'error' | 'skip'
 
 export type TSinchHorizontalStepperItemElement = HTMLElement & {
+  /** Label */
   label: string,
+  /** Description */
   description: string | null,
+  /** Status */
   status: TSinchHorizontalStepperStatusType | null,
+  /** Label */
   setAttribute(name: 'label', value: string): void,
+  /** Description */
   setAttribute(name: 'description', value: string): void,
+  /** Status */
   setAttribute(name: 'status', value: TSinchHorizontalStepperStatusType): void,
 }
 
 export type TSinchHorizontalStepperItemReact = TSinchElementReact<TSinchHorizontalStepperItemElement> & {
+  /** Label */
   label: string,
+  /** Description */
   description?: string,
+  /** Status */
   status?: TSinchHorizontalStepperStatusType,
 }
--- components/horizontal-stepper/types.ts
+++ components/horizontal-stepper/types.ts
@@ -1,11 +1,15 @@
 import type { TSinchElementReact } from '../types'
 
 export type TSinchHorizontalStepperElement = HTMLElement & {
+  /** Current item index, starting from 1 */
   index: string,
+  /** Current item index, starting from 1 */
   setAttribute(name: 'index', value: string): void,
 }
 
 export type TSinchHorizontalStepperReact = TSinchElementReact<TSinchHorizontalStepperElement> & {
+  /** Current item index, starting from 1 */
   index: string,
+  /** Label that is used for a11y */
   'aria-label': string,
 }
