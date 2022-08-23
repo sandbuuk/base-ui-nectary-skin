2799d004d70d631eb943cef4e32cfcd075a55221
Kir Belevich <kir.belevich@sinch.com>
2022-08-23 11:40:12 +0200

    📝 add docs for VerticalStepper

--- components/vertical-stepper-item/types.ts
+++ components/vertical-stepper-item/types.ts
@@ -3,16 +3,25 @@ import type { TSinchElementReact } from '../types'
 export type TSinchVerticalStepperStatusType = 'error' | 'skip'
 
 export type TSinchVerticalStepperItemElement = HTMLElement & {
+  /** Label */
   label: string,
+  /** Description */
   description: string | null,
+  /** Status */
   status: TSinchVerticalStepperStatusType | null,
+  /** Label */
   setAttribute(name: 'label', value: string): void,
+  /** Description */
   setAttribute(name: 'description', value: string): void,
+  /** Status */
   setAttribute(name: 'status', value: TSinchVerticalStepperStatusType): void,
 }
 
 export type TSinchVerticalStepperItemReact = TSinchElementReact<TSinchVerticalStepperItemElement> & {
+  /** Label */
   label: string,
+  /** Description */
   description?: string,
+  /** Status */
   status?: TSinchVerticalStepperStatusType,
 }
