c11c9c58b9d0f381709bbc87cdbfc44ec76dc4eb
Kir Belevich <kir.belevich@sinch.com>
2022-08-26 15:58:16 +0200

    📝 fix TimePicker examples to use modal Popover

--- docs/src/pages/Components/TimePicker/examples/Composition.tsx
+++ docs/src/pages/Components/TimePicker/examples/Composition.tsx
@@ -29,6 +29,7 @@ export const CompositionExample: FC = () => {
       open={isOpen}
       orientation="bottom-left"
       aria-label="Time input"
+      modal
       on-close={onClose}
     >
       <sinch-input
