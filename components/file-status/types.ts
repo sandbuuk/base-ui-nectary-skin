c682678e1a075c2944187833e5fb49dc26bb6dc4
Kir Belevich <kir.belevich@sinch.com>
2022-09-09 12:02:12 +0200

    📝 add FileStatus docs

--- components/file-status/types.ts
+++ components/file-status/types.ts
@@ -3,13 +3,19 @@ import type { TSinchElementReact } from '../types'
 export type TSinchFileStatusType = 'pending' | 'loading' | 'progress' | 'success' | 'error'
 
 export type TSinchFileStatusElement = HTMLElement & {
+  /** File status type */
   type: TSinchFileStatusType,
+  /** File name */
   filename: string,
+  /** File status type */
   setAttribute(name: 'type', value: TSinchFileStatusType): void,
+  /** File name */
   setAttribute(name: 'filename', value: string): void,
 }
 
 export type TSinchFileStatusReact = TSinchElementReact<TSinchFileStatusElement> & {
+  /** File status type */
   type: TSinchFileStatusType,
+  /** File name */
   filename: string,
 }
