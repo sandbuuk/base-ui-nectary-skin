2e0c80336d31bbe45de101bf3feaa4a86e5640d9
Alex Feinstein <alexander.feinstein@sinch.com>
2022-09-27 16:04:08 +0200

    ➕ add `sinch-select-button` component

---
+++ components/select-button/types.ts
@@ -0,0 +1,45 @@
+import type { TSinchElementReact } from '../types'
+
+export type TSinchSelectButtonElement = HTMLElement & {
+  /** Value */
+  text: string,
+  /** Text that appears in the text field when it has no value set */
+  placeholder: string | null,
+  /** Invalid state */
+  invalid: boolean,
+  /** Disabled */
+  disabled: boolean,
+  /** Click event */
+  addEventListener(type: '-click', listener: (e: CustomEvent<void>) => void): void,
+  /** Focus event */
+  addEventListener(type: '-focus', listener: (e: CustomEvent<void>) => void): void,
+  /** Blur event */
+  addEventListener(type: '-blur', listener: (e: CustomEvent<void>) => void): void,
+  /** Value */
+  setAttribute(name: 'text', value: string): void,
+  /** Text that appears in the text field when it has no value set */
+  setAttribute(name: 'placeholder', value: string): void,
+  /** Invalid state */
+  setAttribute(name: 'invalid', value: ''): void,
+  /** Disabled */
+  setAttribute(name: 'disabled', value: ''): void,
+}
+
+export type TSinchSelectButtonReact = TSinchElementReact<TSinchSelectButtonElement> & {
+  /** Controlled value, doesn't change on its own and requres an onChange-value state loop */
+  text: string,
+  /** Label that is used for a11y – might be different from `label` */
+  'aria-label': string,
+  /** Text that appears in the text field when it has no value set */
+  placeholder?: string,
+  /** Invalid state */
+  invalid?: boolean,
+  /** Disabled */
+  disabled?: boolean,
+  /** Click handler */
+  'on-click'?: (e: CustomEvent<void>) => void,
+  /** Focus handler */
+  'on-focus'?: (e: CustomEvent<void>) => void,
+  /** Blur handler */
+  'on-blur'?: (e: CustomEvent<void>) => void,
+}
