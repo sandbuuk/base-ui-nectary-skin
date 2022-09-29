5f3320184b9b4f1f85ef153a5fce8b95ba55353a
Kir Belevich <kir.belevich@sinch.com>
2022-09-29 11:16:04 +0200

    📝 add ActionMenu docs

--- components/action-menu/types.ts
+++ components/action-menu/types.ts
@@ -1,11 +1,15 @@
 import type { TSinchElementReact } from '../types'
 
 export type TSinchActionMenuElement = HTMLElement & {
+  /** How many rows to show and scroll the rest */
   rows: number | null,
+  /** How many rows to show and scroll the rest */
   setAttribute(name: 'rows', value: string): void,
 }
 
 export type TSinchActionMenuReact = TSinchElementReact<TSinchActionMenuElement> & {
+  /** How many rows to show and scroll the rest */
   rows?: number,
+  /** Label that is used for a11y */
   'aria-label': string,
 }
