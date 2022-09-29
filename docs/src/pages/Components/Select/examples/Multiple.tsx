d386ebcf7dd5481a12598a0b1a02ed825b5e7276
Kir Belevich <kir.belevich@sinch.com>
2022-09-29 11:19:09 +0200

    📝 update Select docs

---
+++ docs/src/pages/Components/Select/examples/Multiple.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import type { FC, CSSProperties } from 'react'
+import '@sinch-engage/nectary/popover'
+import '@sinch-engage/nectary/select-menu'
+import '@sinch-engage/nectary/select-menu-option'
+import '@sinch-engage/nectary/select-button'
+import '@sinch-engage/nectary/icons/laptop'
+import '@sinch-engage/nectary/icons/smartphone'
+import '@sinch-engage/nectary/icons/tablet'
+import '@sinch-engage/nectary/icons/watch'
+
+const selectStyles: CSSProperties = {
+  width: 200,
+}
+
+const icons: Record<string, JSX.Element> = {
+  Laptop: <sinch-icon-laptop slot="icon"/>,
+  Smartphone: <sinch-icon-smartphone slot="icon"/>,
+  Tablet: <sinch-icon-tablet slot="icon"/>,
+  Watch: <sinch-icon-watch slot="icon"/>,
+}
+
+export const MultipleExample: FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [value, setValue] = useState('')
+  const onClose = () => setIsOpen(false)
+  const onOpen = () => setIsOpen(true)
+  const onChange = (e: CustomEvent<string>) => setValue(e.detail)
+
+  return (
+    <sinch-popover
+      open={isOpen}
+      aria-label="Select"
+      orientation="bottom"
+      modal
+      on-close={onClose}
+    >
+      <sinch-select-button
+        slot="target"
+        text={value.includes(',') ? 'Multiple selected' : value}
+        placeholder="Select option"
+        aria-label="Open select"
+        style={selectStyles}
+        on-click={onOpen}
+      >
+        {icons[value] ?? null}
+      </sinch-select-button>
+      <sinch-select-menu
+        slot="content"
+        aria-label="Action menu"
+        value={value}
+        multiple
+        on-change={onChange}
+      >
+        <sinch-select-menu-option
+          text="Laptop"
+          aria-label="Laptop option"
+          value="Laptop"
+        >
+          {icons.Laptop}
+        </sinch-select-menu-option>
+        <sinch-select-menu-option
+          text="Smartphone"
+          aria-label="Smartphone option"
+          value="Smartphone"
+        >
+          {icons.Smartphone}
+        </sinch-select-menu-option>
+        <sinch-select-menu-option
+          text="Tablet"
+          aria-label="Tablet option"
+          value="Tablet"
+          disabled
+        >
+          {icons.Tablet}
+        </sinch-select-menu-option>
+        <sinch-select-menu-option
+          text="Watch"
+          aria-label="Watch option"
+          value="Watch"
+        >
+          {icons.Watch}
+        </sinch-select-menu-option>
+      </sinch-select-menu>
+    </sinch-popover>
+  )
+}
