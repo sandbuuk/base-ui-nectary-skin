d87ef87bc4f9a2121e347560806c2bc02f60aaef
Kir Belevich <kir.belevich@sinch.com>
2022-09-29 11:17:26 +0200

    🛠 fix `example-code-loader` to handle string templates

--- libs/example-code-loader/src/index.js
+++ libs/example-code-loader/src/index.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = function(src) {
   const exportedName = path.basename(this.resourcePath, path.extname(this.resourcePath))
 
-  return `${src}\n${exportedName}Example.toString = () => \`${src}\``
+  return `${src}\n${exportedName}Example.toString = () => atob('${btoa(src)}')`
 }
