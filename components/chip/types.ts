98310714d1d12ef14b7dd3387bee9823517eef5f
Kir Belevich <kir.belevich@sinch.com>
2022-09-30 12:45:43 +0200

    📝 add Chip docs

--- components/chip/types.ts
+++ components/chip/types.ts
@@ -8,8 +8,11 @@ export type TSinchChipElement = HTMLElement & {
   color: TSinchColorName | null,
   /** Small */
   small: boolean,
+  /** Click event */
   addEventListener(type: '-click', listener: (e: CustomEvent<void>) => void): void,
+  /** Focus event */
   addEventListener(type: '-focus', listener: (e: CustomEvent<void>) => void): void,
+  /** Blur event */
   addEventListener(type: '-blur', listener: (e: CustomEvent<void>) => void): void,
   /** Text */
   setAttribute(name: 'text', value: string): void,
@@ -26,8 +29,11 @@ export type TSinchChipReact = TSinchElementReact<TSinchChipElement> & {
   color?: TSinchColorName,
   /** Small */
   small?: boolean,
+  /** Click event handler */
   'on-click'?: (e: CustomEvent<void>) => void,
+  /** Focus event handler */
   'on-focus'?: (e: CustomEvent<void>) => void,
+  /** Blur event handler */
   'on-blur'?: (e: CustomEvent<void>) => void,
   /** Label that is used for a11y` */
   'aria-label': string,
