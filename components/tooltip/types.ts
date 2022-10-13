fae6be15003b748b9183ef9b796e9e39836e3268
Kir Belevich <kir.belevich@sinch.com>
2022-10-13 11:14:22 +0200

    📝 add Tooltip docs

--- components/tooltip/types.ts
+++ components/tooltip/types.ts
@@ -3,21 +3,33 @@ import type { TRect, TSinchElementReact } from '../types'
 export type TSinchTooltipOrientation = 'top' | 'bottom' | 'left' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
 
 export type TSinchTooltipElement = HTMLElement & {
+  /** Text */
   text: string,
+  /** @deprecated */
   width: number | null,
+  /** Inverted */
   inverted: boolean,
+  /** Orientation, where it *points to* from origin */
   orientation: TSinchTooltipOrientation,
   readonly footprintRect: TRect,
   readonly tooltipRect: TRect,
+  /** Text */
   setAttribute(name: 'text', value: string): void,
+  /** @deprecated */
   setAttribute(name: 'width', value: string): void,
+  /** Inverted */
   setAttribute(name: 'inverted', value: ''): void,
+  /** Orientation, where it *points to* from origin */
   setAttribute(name: 'orientation', value: TSinchTooltipOrientation): void,
 }
 
 export type TSinchTooltipReact = TSinchElementReact<TSinchTooltipElement> & {
+  /** Text */
   text: string,
+  /** @deprecated */
   width?: number,
+  /** Inverted */
   inverted?: boolean,
+  /** Orientation, where it *points to* from origin */
   orientation?: TSinchTooltipOrientation,
 }
