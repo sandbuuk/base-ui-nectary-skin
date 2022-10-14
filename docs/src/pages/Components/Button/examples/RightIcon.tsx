a0e2cf50e867551779f6e16c147fe4f32a66452e
Kir Belevich <kir.belevich@sinch.com>
2022-10-14 17:03:56 +0200

    📝 tweak various component docs

---
+++ docs/src/pages/Components/Button/examples/RightIcon.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react'
+import '@sinch-engage/nectary/button'
+import '@sinch-engage/nectary/icons/open-in-new'
+
+export const RightIconExample: FC = () => (
+  <sinch-button
+    text="Click"
+    aria-label="Click"
+    type="primary"
+    on-click={() => console.log('click')}
+  >
+    <sinch-icon-open-in-new slot="right-icon"/>
+  </sinch-button>
+)
