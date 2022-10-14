6957cf50c744f49db0175fdd6a42e46a52201dd9
Kir Belevich <kir.belevich@sinch.com>
2022-10-14 16:01:21 +0200

    📝 add Accordion docs

--- components/accordion-item/types.ts
+++ components/accordion-item/types.ts
@@ -3,22 +3,37 @@ import type { TSinchElementReact } from '../types'
 export type TSinchAccordionStatusType = 'info' | 'success' | 'warn' | 'error'
 
 export type TSinchAccordionItemElement = HTMLElement & {
+  /** Value */
   value: string,
+  /** Label */
   label: string,
+  /** Optional text on the right side */
   optionalText: string | null,
-  disabled: boolean,
+  /** Status */
   status: TSinchAccordionStatusType | null,
+  /** Disabled */
+  disabled: boolean,
+  /** Value */
   setAttribute(name: 'value', value: string): void,
+  /** Label */
   setAttribute(name: 'label', value: string): void,
+  /** Optional text on the right side */
   setAttribute(name: 'optionaltext', value: string): void,
-  setAttribute(name: 'disabled', value: ''): void,
+  /** Status */
   setAttribute(name: 'status', value: TSinchAccordionStatusType): void,
+  /** Disabled */
+  setAttribute(name: 'disabled', value: ''): void,
 }
 
 export type TSinchAccordionItemReact = TSinchElementReact<TSinchAccordionItemElement> & {
+  /** Value */
   value: string,
+  /** Label */
   label: string,
+  /** Optional text on the right side */
   optionalText?: string,
-  disabled?: boolean,
+  /** Status */
   status?: TSinchAccordionStatusType,
+  /** Disabled */
+  disabled?: boolean,
 }
