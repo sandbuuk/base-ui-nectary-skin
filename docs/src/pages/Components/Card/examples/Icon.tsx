d156f357bab1ae6b8a407c22b511fd0b3b0917c8
Kir Belevich <kir.belevich@sinch.com>
2022-10-14 16:57:38 +0200

    📝 add Card docs

---
+++ docs/src/pages/Components/Card/examples/Icon.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react'
+import '@sinch-engage/nectary/card'
+import '@sinch-engage/nectary/icons-branded/chatbot'
+
+export const IconExample: FC = () => (
+  <sinch-card
+    caption="Card Title"
+    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+  >
+    <sinch-icon-branded-chatbot slot="icon"/>
+  </sinch-card>
+)
---
+++ docs/src/pages/Components/Card/examples/Link.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react'
+import '@sinch-engage/nectary/card'
+import '@sinch-engage/nectary/link'
+import '@sinch-engage/nectary/icons-branded/chatbot'
+
+export const LinkExample: FC = () => (
+  <sinch-card
+    caption="Card Title"
+    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+  >
+    <sinch-icon-branded-chatbot slot="icon"/>
+    <sinch-link
+      slot="action"
+      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      text="Click here"
+      aria-label="Link"
+      standalone
+    />
+  </sinch-card>
+)
