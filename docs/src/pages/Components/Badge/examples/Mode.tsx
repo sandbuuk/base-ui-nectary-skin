01b2913faf0f5e872d577a060cb7af9ae9c21d56
Kir Belevich <kir.belevich@sinch.com>
2022-10-21 11:35:57 +0200

    📝 add Badge docs

---
+++ docs/src/pages/Components/Badge/examples/Mode.tsx
@@ -0,0 +1,39 @@
+import type { CSSProperties, FC } from 'react'
+import '@sinch-engage/nectary/badge'
+import '@sinch-engage/nectary/avatar'
+import '@sinch-engage/nectary/button'
+import '@sinch-engage/nectary/icon-button'
+import '@sinch-engage/nectary/icons/notifications'
+
+const wrapperStyles: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: 10,
+}
+
+export const ModeExample: FC = () => (
+  <div style={wrapperStyles}>
+    <sinch-badge text="9" size="l" mode="circle">
+      <sinch-avatar src="https://i.pravatar.cc/300" size="l" alt="AB"/>
+    </sinch-badge>
+    <sinch-icon-button aria-label="Notifications">
+      <sinch-badge
+        text="9"
+        size="m"
+        mode="square"
+        slot="icon"
+      >
+        <sinch-icon-notifications/>
+      </sinch-badge>
+    </sinch-icon-button>
+    <sinch-badge text="9" size="l" mode="square">
+      <sinch-button
+        text="Click"
+        type="cta-secondary"
+        aria-label="Click"
+        on-click={() => console.log('click')}
+      />
+    </sinch-badge>
+  </div>
+)
