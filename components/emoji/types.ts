cea5d7f8a54772c003a78848817a2009c4e3ca27
Kir Belevich <kir.belevich@sinch.com>
2022-11-01 10:52:45 +0100

    📝 add Emoji docs

--- components/emoji/types.ts
+++ components/emoji/types.ts
@@ -1,9 +1,13 @@
 import type { TSinchElementReact } from '../types'
 
 export type TSinchEmojiElement = HTMLElement & {
+  /** Emoji character */
   char: string,
+  /** Emoji character */
+  setAttribute(name: 'char', value: string): void,
 }
 
 export type TSinchEmojiReact = TSinchElementReact<TSinchEmojiElement> & {
+  /** Emoji character */
   char: string,
 }
---
+++ docs/src/pages/Components/Emoji/examples/Simple.tsx
@@ -0,0 +1,6 @@
+import type { FC } from 'react'
+import '@sinch-engage/nectary/emoji'
+
+export const SimpleExample: FC = () => (
+  <sinch-emoji char="😀"/>
+)
