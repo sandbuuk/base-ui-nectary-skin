3e084842304f3f0a5c0524fc39c007f518fede79
Alex Feinstein <alexander.feinstein@sinch.com>
2022-11-21 12:02:31 +0100

    🛠️ refactor `sinch-stop-events`

--- components/stop-events/index.ts
+++ components/stop-events/index.ts
@@ -1,25 +1,25 @@
 import { defineCustomElement, unpackCsv } from '../utils'
 
 defineCustomElement('sinch-stop-events', class extends HTMLElement {
+  #controller: AbortController | null = null
   constructor() {
     super()
     this.style.display = 'contents'
   }
 
   connectedCallback() {
+    this.#controller = new AbortController()
+
+    const { signal } = this.#controller
     const events = unpackCsv(this.getAttribute('events')!)
 
     for (const event of events) {
-      this.addEventListener(event, this.#stopEvent)
+      this.addEventListener(event, this.#stopEvent, { signal })
     }
   }
 
   disconnectedCallback() {
-    const events = unpackCsv(this.getAttribute('events')!)
-
-    for (const event of events) {
-      this.removeEventListener(event, this.#stopEvent)
-    }
+    this.#controller!.abort()
   }
 
   #stopEvent = (e: Event) => {
