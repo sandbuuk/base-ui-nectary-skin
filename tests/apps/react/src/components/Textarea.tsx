d1390a4c98bbb67bd75c2896f1be39a245f80fc9
Alex Feinstein <alexander.feinstein@sinch.com>
2022-12-29 11:08:38 +0100

    ➕ add `bottom` slot to `sinch-textarea`

--- tests/apps/react/src/components/Textarea.tsx
+++ tests/apps/react/src/components/Textarea.tsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import type { FC } from 'react'
 import '@sinch-engage/nectary/textarea'
+import '@sinch-engage/nectary/icon-button'
+import '@sinch-engage/nectary/button'
+import '@sinch-engage/nectary/icons/mood'
+import '@sinch-engage/nectary/icons/attach-file'
+import '@sinch-engage/nectary/icons/search'
+import '@sinch-engage/nectary/icons/add-comment'
+import '@sinch-engage/nectary/icons/more-horiz'
+import '@sinch-engage/nectary/icons/send'
 
 type TTextarea = {
   search: URLSearchParams,
@@ -24,7 +32,8 @@ export const Textarea: FC<TTextarea> = ({ search }) => {
 
     return val !== null ? parseInt(val) : undefined
   })()
-  const isResizable = search.get('resizable') === 'true'
+  const isResizable = search.get('resizable') !== null
+  const hasBottom = search.get('bottom') !== null
 
   return (
     <sinch-textarea
@@ -38,6 +47,40 @@ export const Textarea: FC<TTextarea> = ({ search }) => {
       on-focus={onFocus}
       on-blur={onBlur}
       aria-label="Textarea"
-    />
+    >
+      {hasBottom && (
+        <>
+          <sinch-icon-button slot="bottom" aria-label="Paperclip">
+            <sinch-icon-attach-file slot="icon"/>
+          </sinch-icon-button>
+          <sinch-icon-button slot="bottom" aria-label="Emoji">
+            <sinch-icon-mood slot="icon"/>
+          </sinch-icon-button>
+          <sinch-icon-button slot="bottom" aria-label="Variables">
+            <sinch-icon-search slot="icon"/>
+          </sinch-icon-button>
+          <sinch-icon-button slot="bottom" aria-label="Comment">
+            <sinch-icon-add-comment slot="icon"/>
+          </sinch-icon-button>
+          <sinch-icon-button slot="bottom" aria-label="Comment">
+            <sinch-icon-more-horiz slot="icon"/>
+          </sinch-icon-button>
+          <sinch-tag
+            slot="bottom"
+            text="400"
+            color="olive"
+            style={{ marginLeft: 'auto' }}
+          />
+          <sinch-button
+            slot="bottom"
+            type="primary"
+            aria-label="Send"
+            text="Send"
+          >
+            <sinch-icon-send slot="right-icon"/>
+          </sinch-button>
+        </>
+      )}
+    </sinch-textarea>
   )
 }
--- tests/apps/vue/src/components/Textarea.vue
+++ tests/apps/vue/src/components/Textarea.vue
@@ -9,19 +9,57 @@
     @--change="onChange"
     @--focus="onFocus"
     @--blur="onBlur">
+    <template v-if="hasBottom">
+      <sinch-icon-button slot="bottom" aria-label="Paperclip">
+        <sinch-icon-attach-file slot="icon"></sinch-icon-attach-file>
+      </sinch-icon-button>
+      <sinch-icon-button slot="bottom" aria-label="Emoji">
+        <sinch-icon-mood slot="icon"></sinch-icon-mood>
+      </sinch-icon-button>
+      <sinch-icon-button slot="bottom" aria-label="Variables">
+        <sinch-icon-search slot="icon"></sinch-icon-search>
+      </sinch-icon-button>
+      <sinch-icon-button slot="bottom" aria-label="Comment">
+        <sinch-icon-add-comment slot="icon"></sinch-icon-add-comment>
+      </sinch-icon-button>
+      <sinch-icon-button slot="bottom" aria-label="Comment">
+        <sinch-icon-more-horiz slot="icon"></sinch-icon-more-horiz>
+      </sinch-icon-button>
+      <sinch-tag
+        slot="bottom"
+        text="400"
+        color="olive"
+        style="margin-left: auto;"
+      ></sinch-tag>
+      <sinch-button
+        slot="bottom"
+        type="primary"
+        aria-label="Send"
+        text="Send"
+      >
+        <sinch-icon-send slot="right-icon"></sinch-icon-send>
+      </sinch-button>
+    </template>
   </sinch-textarea>
 </template>
 
 <script>
 import '@sinch-engage/nectary/textarea'
+import '@sinch-engage/nectary/button'
+import '@sinch-engage/nectary/icon-button'
+import '@sinch-engage/nectary/tag'
+import '@sinch-engage/nectary/icons/attach-file'
+import '@sinch-engage/nectary/icons/send'
+import '@sinch-engage/nectary/icons/mood'
+import '@sinch-engage/nectary/icons/search'
+import '@sinch-engage/nectary/icons/add-comment'
+import '@sinch-engage/nectary/icons/more-horiz'
 
 export default {
   methods: {
     onChange(e) {
-      if (this.isControlled) {
-        this.value = e.detail
-        window.dispatchEvent(new CustomEvent('sinch-textarea-change', {detail: e.detail}))
-      }
+      this.value = e.detail
+      window.dispatchEvent(new CustomEvent('sinch-textarea-change', {detail: e.detail}))
     },
     onFocus() {
       window.dispatchEvent(new CustomEvent('sinch-textarea-focus'))
@@ -43,8 +81,8 @@ export default {
     isDisabled() {
       return this.search.get('disabled') !== null
     },
-    isControlled() {
-      return this.search.get('uncontrolled') === null
+    hasBottom() {
+      return this.search.get('bottom') !== null
     },
     rows() {
       return this.search.get('rows')
