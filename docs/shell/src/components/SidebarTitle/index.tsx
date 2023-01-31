8e9c0f1dcc4c9c8d31c3af60453e78f8242c7961
Alex Feinstein <alexander.feinstein@sinch.com>
2023-01-31 11:13:35 +0100

    🛠️ redirect `root` to `intro` path

--- docs/shell/src/components/SidebarTitle/index.tsx
+++ docs/shell/src/components/SidebarTitle/index.tsx
@@ -1,29 +1,22 @@
 import '@sinch-engage/nectary/logo/sinch-icon'
 import '@sinch-engage/nectary/title'
-import { Link } from 'react-router-dom'
 import { VersionSelect } from '../VersionSelect'
-import { useNavigateLink } from '~/hooks'
 import './styles.css'
 
 export const SidebarTitle = () => {
-  const { to, isActive } = useNavigateLink('/')
-  const activeClass = isActive === true ? 'selected' : ''
-
   return (
     <div className="sidebar-title">
-      <Link className="sidebar-title-link" to={to}>
-        <sinch-logo-sinch-icon size={24}/>
-        <div className={`sidebar-title-version ${activeClass}`}>
-          <sinch-title
-            class="sidebar-title-version-text"
-            text="Nectary"
-            type="m"
-            level="3"
-            ellipsis
-          />
-          <VersionSelect/>
-        </div>
-      </Link>
+      <sinch-logo-sinch-icon size={24}/>
+      <div className="sidebar-title-version">
+        <sinch-title
+          class="sidebar-title-version-text"
+          text="Nectary"
+          type="m"
+          level="3"
+          ellipsis
+        />
+        <VersionSelect/>
+      </div>
     </div>
   )
 }
