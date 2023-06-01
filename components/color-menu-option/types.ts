b079d043e74fa6ebd0b61f1da805636540416f8d
Alex Feinstein <alexander.feinstein@sinch.com>
2023-06-01 13:03:12 +0200

    🛠️ fix type in color-menu-option

--- components/color-menu-option/types.ts
+++ components/color-menu-option/types.ts
@@ -3,6 +3,8 @@ import type { TSinchElementReact } from '../types'
 export type TSinchColorMenuOptionElement = HTMLElement & {
   /** Value */
   value: string,
+  /** Value */
+  setAttribute(name: 'value', value: string): void,
 }
 
 export type TSinchColorMenuOptionReact = TSinchElementReact<TSinchColorMenuOptionElement> & {
