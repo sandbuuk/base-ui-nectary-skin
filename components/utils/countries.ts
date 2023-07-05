bf33e6ba4b2580493814f13c05a89dcf3f524f2b
Alex Feinstein <alexander.feinstein@sinch.com>
2023-07-05 17:04:50 +0200

    🛠 fix lint issues

--- components/utils/countries.ts
+++ components/utils/countries.ts
@@ -3,7 +3,7 @@ import countriesJson from './countries.json'
 export type TSinchCountry = {
   name: string,
   phoneCode: string,
-  phoneMask: string | null
+  phoneMask: string | null,
 }
 
 export const countries = countriesJson as Record<string, TSinchCountry>
