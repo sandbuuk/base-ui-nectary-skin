2eed252739005234662dc8079fbca930faf1ea65
Alex Feinstein <alexander.feinstein@sinch.com>
2023-08-30 10:28:39 +0200

    ➕ add facebook channel icons

---
+++ assets/icons-channel/facebook/index.ts
@@ -0,0 +1,18 @@
+import { defineCustomElement } from '../../utils/element'
+import { createIconClass } from '../create-icon-class'
+import templateHTML from './template.html'
+import type { TSinchIconChannelElement, TSinchIconChannelReact } from '../types'
+
+defineCustomElement('sinch-icon-channel-facebook', createIconClass(templateHTML))
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'sinch-icon-channel-facebook': TSinchIconChannelReact,
+    }
+  }
+
+  interface HTMLElementTagNameMap {
+    'sinch-icon-channel-facebook': TSinchIconChannelElement,
+  }
+}
