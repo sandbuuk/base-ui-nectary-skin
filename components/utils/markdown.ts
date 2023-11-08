a316a8609c3d45c0c9d92bc14c1ec724ada6aa97
Alex Feinstein <alexander.feinstein@sinch.com>
2023-11-08 10:40:10 +0100

    🛠 refactor markdown parser

--- components/utils/markdown.ts
+++ components/utils/markdown.ts
@@ -1,195 +1,211 @@
-import { filterXSS } from 'xss'
-
-// const contextTags: Record<string, [string, string]> = {
-//   em1: ['<sinch-text type="m" emphasized inline>', '</sinch-text>'],
-//   em2: ['<sinch-text type="m" strong inline>', '</sinch-text>'],
-//   em3: ['<sinch-text type="m" emphasized strong inline>', '</sinch-text>'],
-//   s: ['<sinch-text type="m" strikethrough inline>', '</sinch-text>'],
-//   p: ['<sinch-text type="m">', '</sinch-text>'],
-// }
-
-const contextTags: Record<string, [string, string]> = {
-  em1: ['<span class="em1">', '</span>'],
-  em2: ['<span class="em2">', '</span>'],
-  em3: ['<span class="em3">', '</span>'],
-  s: ['<span class="strikethrough">', '</span>'],
-  p: ['<p class="paragraph">', '</p>'],
+export type TMarkdownInlineParams = {
+  isBold?: boolean,
+  isItalic?: boolean,
+  isStrikethrough?: boolean,
 }
 
-const getContextTags = (token: string): [string, string] | null => {
-  switch (token) {
-    case '_':
-    case '*':
-      return contextTags.em1
-    case '__':
-    case '**':
-      return contextTags.em2
-    case '___':
-    case '***':
-      return contextTags.em3
-    case '~~':
-      return contextTags.s
-    case 'p':
-      return contextTags.p
-    default:
-      return null
-  }
+export type TMarkdownParseVisitor = {
+  link(text: string, href: string): void,
+  emoji(emojiChar: string): void,
+  codetag(text: string): void,
+  inline(text: string, params: TMarkdownInlineParams): void,
+  linebreak(): void,
+  paragraph(): void,
+  list(isOrdered: boolean): void,
+  endList(): void,
+  listItem(): void,
+  end(): Node,
 }
 
-const encodeAttr = (str: string) => {
-  return (`${str}`).replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+const regLinebreak = /(?:<br>\n|<br>|\n)/
+const regParagraph = /\n{2,}/
+const regEm3Star = /\*\*\*(?<em3>.+?)\*\*\*/
+const regEm2Star = /\*\*(?<em2>.+?)\*\*/
+const regEm1Star = /\*(?<em1>.+?)\*/
+const regEm3Underscore = /___(?<em3>.+?)___/
+const regEm2Underscore = /__(?<em2>.+?)__/
+const regEm1Underscore = /_(?<em1>.+?)_/
+const regCodeTag = /`(?<code>.+?)`/
+const regStrikethrough = /~~(?<strike>.+?)~~/
+const regLink = /!?\[(?<linktext>[^\]]*?)\]\((?<linkhref>[^)]+?)\)/
+const regEmoji = /(?<emoji>(?![0-9*#])\p{Emoji})/u
+const regUList = /^(?<indent>[\t ]*?)[*+-][\t ]+(?<ultext>.*?)[\t ]*?$/
+const regOList = /^(?<indent>[\t ]*?)\d+\.[\t ]+(?<oltext>.*?)[\t ]*?$/
+
+const allRegs = [
+  regCodeTag,
+  regLink,
+  regEm3Star,
+  regEm2Star,
+  regEm1Star,
+  regEm3Underscore,
+  regEm2Underscore,
+  regEm1Underscore,
+  regStrikethrough,
+  regEmoji,
+]
+
+export const isEmojiString = (data: string) => regEmoji.test(data)
+
+const excludeRegs = (regs: RegExp[], ...excluding: RegExp[]) => regs.filter((r) => !excluding.includes(r))
+
+const matchClosest = (regs: RegExp[], line: string): RegExpExecArray | null => {
+  return regs
+    .map((reg) => reg.exec(line))
+    .reduce<RegExpExecArray | null>((res, match) => {
+    if (match === null) {
+      return res
+    }
+
+    if (res === null || res.index > match.index) {
+      return match
+    }
+
+    return res
+  }, null)
 }
 
-const tokenizer = /(?<em>___|\*\*\*|__|\*\*|[_*]|~~)|(?:!?\[(?<linktext>[^\]]*?)\]\((?<linkhref>[^)]+?)\))|(?:`(?<code>[^`].*?)`)|(?<paragraph>\n{2,})|(?<newline>\n)/gm
+const INITIAL_CONTEXT: Readonly<TMarkdownInlineParams> = {
+  isBold: false,
+  isItalic: false,
+  isStrikethrough: false,
+}
 
-export const parseMarkdown = (md: string): string => {
-  const context: {token: string, out: string}[] = []
-  let out = ''
-  let lastIndex = 0
+const createLineParser = (visitor: Readonly<TMarkdownParseVisitor>) =>
+  function parseLine(regs: RegExp[], md: string, context: Readonly<TMarkdownInlineParams> = INITIAL_CONTEXT) {
+    let line = md
+    let match: RegExpExecArray | null = null
 
-  const isContextEnd = (token: string) => {
-    return context.length > 0 && context.at(-1)!.token === token
-  }
+    while ((match = matchClosest(regs, line)) !== null) {
+      const groups = match.groups!
+      const matchedStr = match[0]
 
-  const pushContext = (token: string) => {
-    const desc = getContextTags(token)
-
-    if (desc === null) {
-      return
-    }
-
-    context.push({ token, out: '' })
-  }
-
-  const popContext = (token: string, prev: string) => {
-    const desc = getContextTags(token)
-
-    if (desc === null) {
-      return token
-    }
-
-    const ctx = context.pop()!
-
-    return `${desc[0]}${ctx.out}${prev}${desc[1]}`
-  }
-
-  const flushContext = (prev: string) => {
-    let str = ''
-
-    while (context.length > 1) {
-      const ctx = context.pop()!
-
-      str = `${ctx.token}${ctx.out}${str}`
-    }
-
-    if (context.length === 0) {
-      pushContext('p')
-    }
-
-    return popContext(context.at(-1)!.token, str + prev)
-  }
-
-  // eslint-disable-next-line no-param-reassign
-  md = md.trim()
-
-  while (true) {
-    const tokens = tokenizer.exec(md)
-
-    if (tokens === null) {
-      break
-    }
-
-    let prev = md.substring(lastIndex, tokens.index)
-    let chunk = tokens[0]
-    const groups = tokens.groups!
-
-    lastIndex = tokenizer.lastIndex
-
-    // Ensure paragraph
-    if (context.length === 0) {
-      pushContext('p')
-    }
-
-    // escaped
-    if (prev.match(/[^\\](\\\\)*\\$/) != null) {
-      prev = prev.substring(0, prev.length - 1)
-
-      context.at(-1)!.out += prev + chunk
-
-      continue
-    }
-
-    // Inline formatting: *em*, **strong** & friends
-    if (groups.em != null) {
-      const isCloseTag = isContextEnd(groups.em)
-
-      if (isCloseTag) {
-        chunk = popContext(groups.em, prev)
-        context.at(-1)!.out += chunk
-
-        continue
+      if (match.index > 0) {
+        visitor.inline(line.substring(0, match.index), context)
       }
 
-      context.at(-1)!.out += prev
-      pushContext(groups.em)
+      line = line.substring(match.index + matchedStr.length)
 
-      continue
+      if (groups.linkhref != null) {
+        visitor.link(groups.linktext, groups.linkhref)
+      }
+
+      if (groups.code != null) {
+        visitor.codetag(groups.code)
+      }
+
+      if (groups.emoji != null) {
+        visitor.emoji(groups.emoji)
+      }
+
+      if (groups.em1 != null) {
+        parseLine(
+          excludeRegs(regs, regEm1Star, regEm1Underscore),
+          groups.em1,
+          { ...context, isItalic: true }
+        )
+      }
+
+      if (groups.em2 != null) {
+        parseLine(
+          excludeRegs(regs, regEm2Star, regEm2Underscore),
+          groups.em2,
+          { ...context, isBold: true }
+        )
+      }
+
+      if (groups.em3 != null) {
+        parseLine(
+          excludeRegs(regs, regEm3Star, regEm3Underscore, regEm2Star, regEm2Underscore, regEm1Star, regEm1Underscore),
+          groups.em3,
+          { ...context, isBold: true, isItalic: true }
+        )
+      }
+
+      if (groups.strike != null) {
+        parseLine(
+          excludeRegs(regs, regStrikethrough),
+          groups.strike,
+          { ...context, isStrikethrough: true }
+        )
+      }
     }
 
-    // Links, Images:
-    if (groups.linkhref != null) {
-      const href = groups.linkhref
-      const text = groups.linktext.length > 0 ? groups.linktext : href
-
-      // chunk = `<sinch-link external href="${encodeAttr(href)}" text="${encodeAttr(text)}"></sinch-link>`
-      chunk = `<a class="link" target="_blank" href="${encodeAttr(href)}">${encodeAttr(text)}</a>`
-      context.at(-1)!.out += prev + chunk
-
-      continue
-    }
-
-    // `code`:
-    if (groups.code != null) {
-      // chunk = `<sinch-code-tag text="${encodeAttr(groups.code)}"></sinch-code-tag>`
-      chunk = `<span class="code">${encodeAttr(groups.code)}</span>`
-      context.at(-1)!.out += prev + chunk
-
-      continue
-    }
-
-    // New line
-    if (groups.newline != null) {
-      chunk = '<br/>'
-      context.at(-1)!.out += prev + chunk
-
-      continue
-    }
-
-    // New paragraph
-    if (groups.paragraph != null) {
-      out += flushContext(prev)
-
-      continue
+    if (line.length > 0) {
+      visitor.inline(line, context)
     }
   }
 
-  out = (out + flushContext(md.substring(lastIndex))).trim()
+const createBlockParser = (visitor: TMarkdownParseVisitor) => {
+  const parseLine = createLineParser(visitor)
+  const listRegs = [regUList, regOList]
+  const getListItemText = (match: RegExpExecArray) => {
+    return match.groups!.ultext ?? match.groups!.oltext
+  }
+  const BLOCK_MODE_PARAGRAPH = 1
+  const BLOCK_MODE_ULIST = 2
+  const BLOCK_MODE_OLIST = 2
 
-  return filterXSS(
-    out,
-    {
-      // whiteList: {
-      //   'sinch-link': ['href', 'text', 'external'],
-      //   'sinch-code-tag': ['text'],
-      //   'sinch-text': ['type', 'emphasized', 'strong', 'strikethrough', 'inline'],
-      //   br: [],
-      // },
-      whiteList: {
-        p: ['class'],
-        span: ['class'],
-        a: ['href', 'class', 'target'],
-        br: [],
-      },
+  return function parseBlock(lines: string[]) {
+    let mode = 0
+
+    for (let i = 0; i < lines.length;++i) {
+      const listMatch = matchClosest(listRegs, lines[i])
+      const isListLine = listMatch !== null
+
+      if (isListLine) {
+        const groups = listMatch.groups!
+        const isOrderedList = groups.oltext != null
+
+        mode = isOrderedList ? BLOCK_MODE_OLIST : BLOCK_MODE_ULIST
+        visitor.list(isOrderedList)
+
+        const listItemInitialIndent = groups.indent
+        let listItemLines = [getListItemText(listMatch)]
+
+        visitor.listItem()
+
+        for (++i; i < lines.length; ++i) {
+          const listItemMatch = isOrderedList
+            ? regOList.exec(lines[i])
+            : regUList.exec(lines[i])
+
+          if (listItemMatch !== null && listItemMatch.groups!.indent === listItemInitialIndent) {
+            parseBlock(listItemLines)
+            listItemLines = [getListItemText(listItemMatch)]
+            visitor.listItem()
+          } else {
+            listItemLines.push(lines[i])
+          }
+        }
+
+        parseBlock(listItemLines)
+        visitor.endList()
+        mode = 0
+      } else {
+        // join parsed lines with linebreaks
+        if (mode === BLOCK_MODE_PARAGRAPH) {
+          visitor.linebreak()
+        }
+
+        // Paragraph
+        if (mode !== BLOCK_MODE_PARAGRAPH) {
+          mode = BLOCK_MODE_PARAGRAPH
+          visitor.paragraph()
+        }
+
+        parseLine(allRegs, lines[i])
+      }
     }
-  )
+  }
+}
+
+export const parseMarkdown = (md: string, visitor: TMarkdownParseVisitor): Node => {
+  const parseBlock = createBlockParser(visitor)
+
+  for (const block of md.trim().split(regParagraph)) {
+    parseBlock(block.trim().split(regLinebreak))
+  }
+
+  return visitor.end()
 }
