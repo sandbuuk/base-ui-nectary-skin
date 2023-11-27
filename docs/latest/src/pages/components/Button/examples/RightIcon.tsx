18118f9a4798e35e6547b5af80ef9bebb889db1e
Alex Feinstein <alexander.feinstein@sinch.com>
2023-11-27 12:32:51 +0100

    📝 update docs

--- docs/latest/src/pages/components/Button/examples/RightIcon.tsx
+++ docs/latest/src/pages/components/Button/examples/RightIcon.tsx
@@ -2,13 +2,13 @@ import type { FC } from 'react'
 import '@nectary/components/button'
 import '@nectary/components/icon'
 
-export const LeftIconExample: FC = () => (
+export const RightIconExample: FC = () => (
   <sinch-button
     text="Click"
     aria-label="Click"
     type="primary"
     on-click={() => console.log('click')}
   >
-    <sinch-icon name="open_in_new" slot="left-icon"/>
+    <sinch-icon name="open_in_new" slot="right-icon"/>
   </sinch-button>
 )
