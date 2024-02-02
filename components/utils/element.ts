4c9d026e29f8ca858d4dcd8047fb4af76834023f
William Högman Rudenmalm <me@whn.se>
2024-02-02 13:26:40 +0100

    bugfix: assert json export for package.json

--- components/utils/element.ts
+++ components/utils/element.ts
@@ -1,4 +1,4 @@
-import pkg from '../package.json'
+import pkg from '../package.json' assert { type: 'json' };
 
 const nectaryDefinitions = new Map<string, CustomElementConstructor>()
 let nectaryRegistry: CustomElementRegistry | null = null
