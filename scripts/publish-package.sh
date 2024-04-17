47e5c944c58cdcb3271b9f8a811353091107a784
William Högman Rudenmalm <me@whn.se>
2024-04-17 23:34:59 +0200

    ci: fix publish script

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Check if the directory and NPM token arguments are provided
-if [ -z "$1" ] then
-  echo "Usage: $0 <npm_token> [ci_commit_tag]"
-  exit 1
-fi
-
-NPM_TOKEN=$1
-CI_COMMIT_TAG=$2
-
-pnpm --dir $DIR build
-
 # Configure npm for publishing
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
