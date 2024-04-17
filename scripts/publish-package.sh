fa17a18fcaf8886f09e7299382893cccbe930e28
William Högman Rudenmalm <me@whn.se>
2024-04-17 21:28:51 +0200

    ci: use semantic release

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Check if the directory and NPM token arguments are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <directory> <npm_token> [ci_commit_tag]"
+if [ -z "$1" ] then
+  echo "Usage: $0 <npm_token> [ci_commit_tag]"
   exit 1
 fi
 
-# Assign arguments to variables
-DIR=$1
-NPM_TOKEN=$2
-CI_COMMIT_TAG=$3  # This is optional, can be empty
+NPM_TOKEN=$1
+CI_COMMIT_TAG=$2
 
 pnpm --dir $DIR build
 
@@ -18,10 +16,8 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
 # Check if CI_COMMIT_TAG is provided and not empty
 if [ ! -z "$CI_COMMIT_TAG" ]; then
-  npm publish $DIR/
-  # Send changelog to Slack
-  node ./scripts/slack-changelog.js $DIR
+  pnpm run dry-run
+  pnpm run release
 else
-  # CI_COMMIT_TAG is not provided, publish with the tag 'next'
-  npm publish $DIR/ --tag next
+  # TODO: What should we do
 fi
