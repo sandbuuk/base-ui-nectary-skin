1c582a4f134cab0cf4194796583fbb8fb3f9ab20
Paul Bricout <paubri@on.sinch.com>
2024-04-24 13:57:30 +0200

    ci: comment out manual line

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -5,7 +5,7 @@ set -eu pipefail
 NPM_TOKEN=$1
 
 # Configure npm for publishing
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+# echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
 pnpm run dry-run
 
