090e250f08c28fded496cb46889c4a6ad42d7f9d
Paul Bricout <paubri@on.sinch.com>
2024-04-24 14:53:30 +0200

    ci: add npm token

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -2,11 +2,10 @@
 
 set -eu pipefail
 
-NPM_TOKEN=$1
+export NPM_TOKEN=$1
 
 # Configure npm for publishing
-# echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 pnpm run dry-run
 
 # # commented out while testing
