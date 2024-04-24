af4236ce080672550ffc8de989c2181b3db25e8c
Paul Bricout <paubri@on.sinch.com>
2024-04-24 10:09:05 +0200

    chore: test commit

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -9,7 +9,7 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
 pnpm run dry-run
 
-if [ "$CI_COMMIT_REF_SLUG" == "$CI_DEFAULT_BRANCH" ]; then
-  # commented out while testing
-  # pnpm run release
-fi
+# # commented out while testing
+# if [ "$CI_COMMIT_REF_SLUG" == "$CI_DEFAULT_BRANCH" ]; then
+#   pnpm run release
+# fi
