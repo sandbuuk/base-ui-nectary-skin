d8f5beac97710b1095740ce6485154840e5024b7
Paul Bricout <paubri@on.sinch.com>
2024-07-15 18:17:59 +0200

    fix(docs): handle slots

---
+++ docs/latest/src/pages/components/ButtonGroup/examples/Slots.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import type { FC } from 'react'
+import '@nectary/components/button-group'
+import '@nectary/components/button-group-item'
+import '@nectary/components/text'
+import '@nectary/components/icon'
+
+export const SlotsExample: FC = () => {
+  const [pressedButton, setPressedButton] = useState(0)
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <sinch-button-group type="cta-secondary">
+        <sinch-button-group-item
+          aria-label="Click"
+          on-click={() => setPressedButton(1)}
+        >
+          <sinch-icon name="mood" slot="icon" />
+        </sinch-button-group-item>
+        <sinch-button-group-item
+          aria-label="button-2"
+          text="button-2"
+          on-click={() => setPressedButton(2)}
+        >
+          <sinch-icon name="open_in_new" slot="right-icon" />
+        </sinch-button-group-item>
+        <sinch-button-group-item
+          aria-label="button-3"
+          text="button-3"
+          on-click={() => setPressedButton(3)}
+        >
+          <sinch-icon name="open_in_new" slot="icon" />
+        </sinch-button-group-item>
+      </sinch-button-group>
+      {pressedButton !== 0 && <sinch-text type="m">Button {pressedButton} pressed!</sinch-text>}
+    </div>
+  )
+}
