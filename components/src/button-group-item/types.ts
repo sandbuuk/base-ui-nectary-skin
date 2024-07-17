1d9ac0bf7d969ca069b3fff7ccee2585b64fd10f
Paul Bricout <paubri@on.sinch.com>
2024-07-17 09:59:35 +0200

    fix(docs): add API page

--- components/src/button-group-item/types.ts
+++ components/src/button-group-item/types.ts
@@ -1,6 +1,20 @@
 import type { TSinchButtonElement, TSinchButtonReact } from '../button/types'
 import type { TSinchElementReact } from '../types'
 
-export type TSinchButtonGroupItemElement = HTMLElement & Pick<TSinchButtonElement, 'text' | 'disabled' | 'toggled' | 'addEventListener' | 'setAttribute'> & {}
+export type TSinchButtonGroupItemElement = HTMLElement & {
+  text: TSinchButtonElement['text'],
+  disabled: TSinchButtonElement['disabled'],
+  toggled: TSinchButtonElement['toggled'],
+  addEventListener: TSinchButtonElement['addEventListener'],
+  setAttribute: TSinchButtonElement['setAttribute'],
+}
 
-export type TSinchButtonGroupItemReact = TSinchElementReact<TSinchButtonGroupItemElement> & Pick<TSinchButtonReact, 'text' | 'disabled' | 'toggled' | 'on-blur' | 'on-click' | 'on-focus' | 'aria-label'> & {}
+export type TSinchButtonGroupItemReact = TSinchElementReact<TSinchButtonGroupItemElement> & {
+  text?: TSinchButtonReact['text'],
+  disabled?: TSinchButtonReact['disabled'],
+  toggled?: TSinchButtonReact['toggled'],
+  'on-blur'?: TSinchButtonReact['on-blur'],
+  'on-click'?: TSinchButtonReact['on-click'],
+  'on-focus'?: TSinchButtonReact['on-focus'],
+  'aria-label': TSinchButtonReact['aria-label'],
+}
