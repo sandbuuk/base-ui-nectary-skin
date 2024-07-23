c33d4da5b0c2515b1b20dec96eeb7b31eed5317b
Paul Bricout <paubri@on.sinch.com>
2024-07-23 16:27:44 +0200

    chore(assets): revert cjs build

--- assets/babel.config.cjs.js
+++
@@ -1,8 +0,0 @@
-const config = require('./babel.config')
-const updatedConfig = { ...config }
-
-updatedConfig.plugins.push('@babel/plugin-transform-modules-commonjs')
-
-module.exports = {
-  ...updatedConfig,
-}
