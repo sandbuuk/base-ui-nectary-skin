412c724d72a7b1733ff40219a08f193e3965013d
Paul Bricout <paubri@on.sinch.com>
2025-01-31 09:07:33 +0100

    fix(assets): un-deprecate usage of icons-channels props

--- assets/icons-channel/types.ts
+++ assets/icons-channel/types.ts
@@ -1,13 +1,10 @@
 import type { TSinchElementReact } from '../types'
 
 export type TSinchIconChannelElement = HTMLElement & {
-  /** @deprecated */
   size: number,
-  /** @deprecated */
   setAttribute(name: 'size', value: string): void,
 }
 
 export type TSinchIconChannelReact = TSinchElementReact<TSinchIconChannelElement> & {
-  /** @deprecated */
   size?: number,
 }
