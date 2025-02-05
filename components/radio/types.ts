d54a7aaddc5d43ef49e0412d1789417cfe82ce27
Blenda Öhman <blenda.ohman@gmail.com>
2025-02-05 14:21:31 +0100

    fix(components): horizontal direction to radio buttons

--- components/radio/types.ts
+++ components/radio/types.ts
@@ -3,7 +3,10 @@ import type { TSinchElementReact } from '../types'
 export type TSinchRadioElement = HTMLElement & {
   value: string,
   invalid: boolean,
-  addEventListener(type: '-change', listener: (e: CustomEvent<boolean>) => void): void,
+  addEventListener(
+    type: '-change',
+    listener: (e: CustomEvent<boolean>) => void
+  ): void,
   setAttribute(name: 'value', value: string): void,
   setAttribute(name: 'invalid', value: ''): void,
 }
@@ -13,4 +16,5 @@ export type TSinchRadioReact = TSinchElementReact<TSinchRadioElement> & {
   invalid?: boolean,
   'aria-label': string,
   'on-change'?: (e: CustomEvent<string>) => void,
+  style?: { '--sinch-comp-radio-direction'?: 'row' | 'column', '--sinch-comp-radio-gap'?: number | string },
 }
