822cc95ef7c9a6bbd65fdced52ae632653693a92
Carl Willman <carl.willman@sinch.com>
2025-03-18 09:33:19 +0100

    ran linter

--- docs/latest/src/pages/components/Tag/examples/Ellipsis.tsx
+++ docs/latest/src/pages/components/Tag/examples/Ellipsis.tsx
@@ -1,18 +1,20 @@
-import type { CSSProperties, FC } from "react";
-import "@nectary/components/tag";
+import type { CSSProperties, FC } from 'react'
+import '@nectary/components/tag'
 
 const wrapperStyles: CSSProperties = {
-  display: "flex",
-  flexWrap: "wrap",
+  display: 'flex',
+  flexWrap: 'wrap',
   gap: 10,
-};
+}
 
 export const EllipsisExample: FC = () => (
   <div style={wrapperStyles}>
+    <div style={{ width: 100 }}>
       <sinch-tag
         color="info"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
       />
+    </div>
     <div style={{ width: 100 }}>
       <sinch-tag
         color="info"
@@ -21,4 +23,4 @@ export const EllipsisExample: FC = () => (
       />
     </div>
   </div>
-);
+)
