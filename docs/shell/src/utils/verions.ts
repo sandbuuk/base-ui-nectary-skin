e76cf16135f94f7d6e2ce3ece19ed414e0f7ad07
Paul Bricout <paubri@on.sinch.com>
2025-07-09 11:21:06 +0200

    feat(utils): sort version keys for consistent ordering

--- docs/shell/src/utils/verions.ts
+++ docs/shell/src/utils/verions.ts
@@ -2,6 +2,20 @@ import { versions } from '../../versions'
 
 export * from '../../versions'
 
-export const versionKeys = Object.keys(versions)
+export const versionKeys = Object.keys(versions).sort((a, b) => {
+  const aParts = a.split('.').map(Number)
+  const bParts = b.split('.').map(Number)
+
+  for (let i = 0; i < Math.max(aParts.length, bParts.length); i++) {
+    const aPart = aParts[i] !== undefined && !Number.isNaN(aParts[i]) ? aParts[i] : 0
+    const bPart = bParts[i] !== undefined && !Number.isNaN(bParts[i]) ? bParts[i] : 0
+
+    if (aPart !== bPart) {
+      return aPart - bPart
+    }
+  }
+
+  return 0
+})
 
 export const latestVersion = versionKeys[versionKeys.length - 1]
