d274012aec355704049f6ef16afebf92e1855b7b
Sergei Varzin <sergei.varzin@sinch.com>
2025-10-13 17:27:36 +0200

    chore(docs): adjusted example colors

--- docs/latest/src/pages/components/IconsChannel/examples/CustomColors.tsx
+++ docs/latest/src/pages/components/IconsChannel/examples/CustomColors.tsx
@@ -6,14 +6,14 @@ import '@nectary/assets/icons-channel/mms-round'
 export const CustomColorsExample: FC = () => {
   const customColors: CSSProperties = {
     '--sinch-comp-icon-channel-color-instagram-bg': 'orange',
-    '--sinch-comp-icon-channel-color-instagram-layera': '#E1306C',
+    '--sinch-comp-icon-channel-color-instagram-layera': '#ff0073',
 
     '--sinch-comp-icon-channel-color-apple-business-chat-bg': 'linear-gradient(135deg, #00A2E8 0%, #4CD964 100%)',
-    '--sinch-comp-icon-channel-color-apple-business-chat-layera': '#96fff8',
+    '--sinch-comp-icon-channel-color-apple-business-chat-layera': '#006356',
 
     '--sinch-comp-icon-channel-color-mms-bg': '#6fcaff',
-    '--sinch-comp-icon-channel-color-mms-layera': '#7175ef',
-    '--sinch-comp-icon-channel-color-mms-layerb': '#2f35d4',
+    '--sinch-comp-icon-channel-color-mms-layera': '#002bad',
+    '--sinch-comp-icon-channel-color-mms-layerb': '#004eea',
   } as CSSProperties
 
   return (
