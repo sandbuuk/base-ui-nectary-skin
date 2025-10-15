aaa0db26bc5fd09280c02a7a05f51ba2eaf36c66
Oliver Carlsson <oliver.carlsson@sinch.com>
2025-10-15 16:22:05 +0200

    chore(tf): Add CORS config to s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,6 +67,17 @@ resource "aws_s3_bucket_policy" "nectary_cdn" {
   })
 }
 
+resource "aws_s3_bucket_cors_configuration" "bucket" {
+  bucket = aws_s3_bucket.nectary_cdn.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
 ################################################################################
 # Cloudfront distribution.
 ################################################################################
